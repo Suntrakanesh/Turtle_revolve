93a05bdd85507a3c73cdb042344bacb9c8a70523
Suntrakanesh SU <64604283+Suntrakanesh@users.noreply.github.com>
2020-10-19 20:38:58 +0530

    Update node_turtle_revolve.py

--- Turtlesim_revolve/node_turtle_revolve.py
+++ Turtlesim_revolve/node_turtle_revolve.py
@@ -2,41 +2,33 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
-
-def callback()
-       global x_pose, y_pose
-	x_pose=posi.x
-	y_pose=posi.y
 
 def turtle_move_circle():
-	pub = rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size = 10)
-	sub = rospy.Subscriber('/turtle1/pose', Pose, callback)
+        pub = rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size = 2)
         rospy.init_node('node_turtle_revolve')
-	vel_msg = Twist()
-	posi = Pose()
+        vel_msg = Twist()
 
-	print "Your wish is my command"
-	print "If you want to quit and watch me draw squares, Ctl-c me"
+        print "Your wish is my command"
+        print "If you want to quit and watch me draw squares, Ctl-c me"
 
 
-	vel_msg.linear.x = 2
-	vel_msg.linear.y = 0
-	vel_msg.linear.z = 0
-	vel_msg.angular.x = 0
-	vel_msg.angular.y = 0
-	vel_msg.angular.z = 2
-	
-	#Move Robot in circle
-	while not rospy.is_shutdown():
-		if x_pose!= 5.544445 && y_pose!=5.544445:
-		   pub.publish(vel_msg)
-		else :
-	            break
+        vel_msg.linear.x = 2
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = 2
+
+        #Move Robot in circle
+        while not rospy.is_shutdown():
+                pub.publish(vel_msg)
 
 
 if __name__ == "__main__":
-	try:
-		turtle_move_circle()
-	except rospy.ROSInterruptException:
-		pass
+        try:
+                turtle_move_circle()
+        except rospy.ROSInterruptException:
+                pass
+
+
+
